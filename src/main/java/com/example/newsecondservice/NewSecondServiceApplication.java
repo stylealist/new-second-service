51947d745720e535e0ad9e6012631020e7278dd2
sungjoong <stylealist@gmail.com>
2024-08-21 20:31:25 +0900

    init commit

---
+++ src/main/java/com/example/newsecondservice/NewSecondServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsecondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSecondServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSecondServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/newsecondservice/SecondServiceController.java
@@ -0,0 +1,25 @@
+package com.example.newsecondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+@Slf4j
+public class SecondServiceController {
+    Environment env;
+
+    @Autowired
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+}
